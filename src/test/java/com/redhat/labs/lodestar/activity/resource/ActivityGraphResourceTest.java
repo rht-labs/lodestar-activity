270106bd17adafb2827c1978b4ede501b74af73c
Kevin McAnoy <kevinmcanoy@gmail.com>
2021-06-23 10:15:26 -0700

    1 graphql test

---
+++ src/test/java/com/redhat/labs/lodestar/activity/resource/ActivityGraphResourceTest.java
@@ -0,0 +1,33 @@
+package com.redhat.labs.lodestar.activity.resource;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import com.redhat.labs.lodestar.activity.mock.ResourceLoader;
+import com.redhat.labs.lodestar.activity.service.ActivityService;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.h2.H2DatabaseTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
+import io.restassured.http.ContentType;
+
+@QuarkusTest
+@QuarkusTestResource(H2DatabaseTestResource.class)
+public class ActivityGraphResourceTest {
+
+    @InjectMock
+    ActivityService service;
+
+    @Test
+    public void testTotalCommits() {
+        Mockito.when(service.getActivityCount()).thenReturn(3L);
+
+        String body = ResourceLoader.load("graphql-activity.json");
+        given().contentType(ContentType.JSON).body(body).when().post("/graphql").then().statusCode(200).assertThat()
+                .body("data.totalCommits", equalTo(3));
+    }
+}
